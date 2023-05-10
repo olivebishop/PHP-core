ca4ee6ada0db9db8d8d8d3a312df82487e8b0090
olivebishop <olivehendrilgen1@gmail.com>
2023-05-10 15:45:51 +0300

    day one

---
+++ operators.php
@@ -0,0 +1,22 @@
+<?php
+
+//1. arithemetic operators
+$deposit = 80000;
+$widthdrawal = 2000;
+$balance = $deposit-$widthdrawal;
+print_r($balance);
+
+echo "<br>";
+
+
+
+
+//2.
+
+
+
+
+
+
+
+?>
---
+++ variable.php
@@ -0,0 +1,50 @@
+<?php
+
+#examples of data types
+// to know which datatypes are used
+//1.init
+$age =19;
+var_dump($age);
+
+echo "<br>";
+
+//2 float
+$deposit = 50000.50;
+var_dump($deposit);
+
+echo "<br>";
+
+//3 string
+$msg ="hello world";
+var_dump($msg);
+
+echo "<br>";
+
+//4 boolean
+$isMarried= true;
+var_dump($isMarried);
+
+echo "<br>";
+
+//5 null
+$status =null;
+var_dump($status);
+
+echo "<br>";
+
+//6 array
+$items =array($items2);
+var_dump($items);
+
+echo "<br>";
+
+
+//7 object
+class payment{
+
+}
+$payment_obj = new payment();
+var_dump($payment_obj);
+
+
+?>
